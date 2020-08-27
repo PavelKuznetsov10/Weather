d1f8af95ed5c13aaea5a24c4198062b99a7854c0
Pavel Kuznetsov <kpp2508@gmail.com>
2020-08-27 09:51:12 +0300

    weat-8

---
+++ app/src/main/java/com/example/weather/InputFragment.kt
@@ -0,0 +1,74 @@
+package com.example.weather
+
+import android.os.Bundle
+import android.text.Editable
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.fragment_input.*
+import kotlinx.android.synthetic.main.fragment_input.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InputFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InputFragment : Fragment(), View.OnClickListener {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val inputView: View = inflater.inflate(R.layout.fragment_input, container, false)
+        val inputCity: Editable = input_city.text
+        val inputLatitude: Editable = input_latitude.text
+        val inputLongitude: Editable = input_longitude.text
+        btn_add.setOnClickListener(this)
+
+        return inputView
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment InputFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            InputFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+}
